be24eb5ca0c43fee62ba5ecb49fc3bfcf88af68a
woody.ljy <liaojiayang@live.com>
2019-01-03 19:58:09 +0800

    卖方时间修改
    假设换房周期最短为2月

--- 换房.sql
+++ 换房.sql
@@ -1,23 +1,4 @@
-# 2018年12月售房客户
-# 主键为手机号码
-
-select 
- distinct(seller_telephone) -- 卖方手机号码
- ,substr(real_sign_time,1,7)
- ,city_code
- ,housedel_id -- 房源ID
- ,house_resblock_id -- 小区ID
- ,agreement_amt
- from
- dw.dw_allinfo_agr_agreement_da
- where
- pt = '20190101000000'
- and substr(real_sign_time,1,7) = "2018-12"
- and city_code = "110000"
- and del_type = "买卖"
-
-
-# 2018年12月换房客户
+# 2018年北京换房客户分析
 
 select * from
 	(select * from
@@ -27,13 +8,15 @@ select * from
 		 ,city_code
 		 ,housedel_id -- 房源ID
 		 ,house_resblock_id -- 小区ID
+		 ,house_resblock_name -- 小区ID
 		 ,house_bizcircle_name -- 商圈
+		 ,house_district_name -- 城区
 		 ,agreement_amt
 		 from
 		 dw.dw_allinfo_agr_agreement_da
 		 where
 		 pt = '20190101000000'
-		 and substr(real_sign_time,1,7) = "2018-12"
+		 and substr(real_sign_time,1,7) >= "2018-01"
 		 and city_code = "110000"
 		 and del_type = "买卖") as tab_a
 		where
@@ -43,7 +26,7 @@ select * from
 				 dw.dw_allinfo_agr_agreement_da
 				 where
 				 pt = '20190101000000'
-				 and substr(real_sign_time,1,7) = "2018-12"
+				 and substr(real_sign_time,1,7) >= "2017-10" -- 假设换房周期最短为2个月
 				 and city_code = "110000"
 				 and del_type = "买卖")
 				 ) as tab_1
@@ -56,13 +39,15 @@ select * from
 	 ,city_code
 	 ,housedel_id -- 房源ID
 	 ,house_resblock_id -- 小区ID
+	 ,house_resblock_name -- 小区ID
 	 ,house_bizcircle_name -- 商圈
+	 ,house_district_name -- 城区
 	 ,agreement_amt
 	 from
 	 dw.dw_allinfo_agr_agreement_da
 	 where
 	 pt = '20190101000000'
-	 and substr(real_sign_time,1,7) = "2018-12"
+	 and substr(real_sign_time,1,7) >= "2017-10" 
 	 and city_code = "110000"
 	 and del_type = "买卖") as tab_2
 
